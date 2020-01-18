ed7998c1dd2fefa2dd130bdbe359d50dc6a3ab44
Domo <stolarczyk.dominik@gmail.com>
2020-01-18 11:05:17 +0100

    +event handlers

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import SearchBar from './components/SearchBar';
 class App extends React.Component {
 	render() {
 		return (
-			<div className="App">
+			<div className="ui container">
 				<SearchBar />
 			</div>
 		);
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,30 @@
 import React, { Component } from 'react';
 
 export class SearchBar extends Component {
+	state = { term: '' };
+
+	onInputChange = e => {
+		this.setState({ term: e.target.value });
+	};
+	onFormSumbit = e => {
+		e.preventDefault();
+	};
+
 	render() {
-		return <div>search bar</div>;
+		return (
+			<div className="search-bar ui segment">
+				<form onSubmit={this.onFormSubmit} className="ui form">
+					<div className="field">
+						<label>video search</label>
+						<input
+							value={this.state.term}
+							onChange={this.onInputChange}
+							type="text"
+						/>
+					</div>
+				</form>
+			</div>
+		);
 	}
 }
 
