ded03d59e884aebc968c1bee6f8dc8e284fdc3ff
Domo <stolarczyk.dominik@gmail.com>
2020-01-18 16:54:34 +0100

    onClick VideoItem

--- src/App.js
+++ src/App.js
@@ -5,19 +5,26 @@ import youtube from './api/youtube';
 import VideoList from './components/VideoList';
 
 class App extends React.Component {
-	state = { videos: [] };
+	state = { videos: [], selectedVideo: null };
 	onTermSubmit = async term => {
 		const response = await youtube.get('/search', {
 			params: { q: term }
 		});
 		this.setState({ videos: response.data.items });
 	};
+
+	onVideoSelect = video => {
+		console.log('video clicked', video);
+	};
 	render() {
 		return (
 			<div className="ui container">
 				<SearchBar onFormSubmit={this.onTermSubmit} />
 				{this.state.videos.length} videos found
-				<VideoList videos={this.state.videos} />
+				<VideoList
+					onVideoSelect={this.onVideoSelect}
+					videos={this.state.videos}
+				/>
 			</div>
 		);
 	}
--- src/components/VideoItem.jsx
+++ src/components/VideoItem.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import './VideoItem.css';
 
-function VideoItem({ video }) {
+function VideoItem({ video, onVideoSelect }) {
 	return (
-		<div className="video-item item">
+		<div onClick={() => onVideoSelect(video)} className="video-item item">
 			<img
 				className="ui image"
 				src={video.snippet.thumbnails.medium.url}
